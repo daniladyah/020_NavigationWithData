7f6f07877dcdb0e17dc0637a26b12cb6c149d241
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2023-11-15 22:27:05 +0700

    menambahkan function Custom Details Screen

---
+++ app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
@@ -0,0 +1,13 @@
+package com.example.esjumbo
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomDetailsScreen(
+    onConfirmButtonClicked: (String, String,String)-> Unit,
+    onCancelButtonClicked: () -> Unit
+){
+
+}
