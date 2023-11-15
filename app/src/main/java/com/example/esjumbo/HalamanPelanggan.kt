872d5410e6814ea06bccae2f3f58b3804ae684de
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2023-11-15 22:48:00 +0700

    menambahkan variabel pelanggan,no telp dan alamat

--- app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
+++ app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
@@ -1,11 +1,24 @@
 package com.example.esjumbo
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -17,4 +30,57 @@ fun CustomDetailsScreen(
     var nomorTelepon by remember{ mutableStateOf("") }
     var alamat by remember{ mutableStateOf("") }
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 16.dp)
+    ){
+        OutlinedTextField(
+            value = namaPelanggan,
+            onValueChange = {namaPelanggan = it},
+            label = {Text(text = " Nama Pelanggan")},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            )
+        OutlinedTextField(
+            value = nomorTelepon,
+            onValueChange = {nomorTelepon = it},
+            label = { Text(text = "Nomor Telepon")},
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Phone
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            )
+        OutlinedTextField(
+            value = alamat,
+            onValueChange ={ alamat = it},
+            label = { Text(text = "Alamat")},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+            OutlinedButton(onClick = onCancelButtonClicked) {
+                Text(text = "Cancel")
+            }
+
+            Button(
+                onClick = {
+                    if (namaPelanggan.isNotEmpty() && nomorTelepon.isNotEmpty() && alamat.isNotEmpty()){
+                        onConfirmButtonClicked(namaPelanggan, nomorTelepon, alamat)
+                    }
+                }
+            ) {
+                Text(text = "Next")
+            }
+        }
+    }
 }
