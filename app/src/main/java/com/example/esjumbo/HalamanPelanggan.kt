0a073dfd1a06579450d5171ea6cddee222b13bfe
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2023-11-15 22:30:24 +0700

    menambahkan variabel pelanggan,no telp dan alamat

--- app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
+++ app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
@@ -2,6 +2,10 @@ package com.example.esjumbo
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -9,5 +13,8 @@ fun CustomDetailsScreen(
     onConfirmButtonClicked: (String, String,String)-> Unit,
     onCancelButtonClicked: () -> Unit
 ){
+    var namaPelanggan by remember{ mutableStateOf("") }
+    var nomorTelepon by remember{ mutableStateOf("") }
+    var alamat by remember{ mutableStateOf("") }
 
 }
